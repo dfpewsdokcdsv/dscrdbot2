362b0f7b8a8dbd927caed3f774f1af7ecd5e488f
dfpewsdokcdsv <132742180+dfpewsdokcdsv@users.noreply.github.com>
2023-05-31 19:58:40 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+    async def on_message(self, message):
+        if message.content.startswith('!deleteme'):
+            msg = await message.channel.send('I will delete myself now...')
+            await msg.delete()
+
+            # this also works
+            await message.channel.send('Goodbye in 3 seconds...', delete_after=3.0)
+
+    async def on_message_delete(self, message):
+        msg = f'{message.author} has deleted the message: {message.content}'
+        await message.channel.send(msg)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+bot.run("token")
